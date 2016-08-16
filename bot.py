eb24a82a1b5287db462dd8e8e2816bd1a2d9175c
Sergei <ValterBishop@gmail.com>
2016-08-16 19:29:39 +0300

    Message edit handler added (Works, but not usefull now) Inline handler added (For now he just shows hint menu with examples of requests) Hints examples in config.json

--- bot.py
+++ bot.py
@@ -4,16 +4,43 @@ import cherrypy
 from config import Config
 from webhook_server import WebhookServer
 import yandex_speechkit
+from telebot import types
 
 # Instantiate our bot
-bot = telebot.AsyncTeleBot(Config.instance().telegram_api_token)
+bot = telebot.TeleBot(Config.instance().telegram_api_token)
+'''replied_messages = {}
 
+@bot.edited_message_handler(func=lambda message: True, content_types=["text"])
+def message_edited_handler(message):
+    msgs = yandex_speechkit.recognize_date(message.text)
+    if(message.message_id in replied_messages):
+        bot.edit_message_text(chat_id=message.chat.id, text=u'Recognized dates:\n' + msgs,
+                              message_id=replied_messages[message.message_id])
+'''
 # Message handler
 @bot.message_handler(func=lambda message: True, content_types=["text"])
-def repeat_all_messages(message):
+def message_income_handler(message):
     msgs = yandex_speechkit.recognize_date(message.text)
-    bot.reply_to(message, u'Recognized dates:\n' + msgs)
+    bot_msg = bot.reply_to(message, u'Recognized dates:\n' + msgs)
+    # replied_messages[message.message_id] = bot_msg.message_id
 
+
+
+
+@bot.inline_handler(lambda query: len(query.query) is 0)
+def default_query(inline_query):
+    hint_msg = "Введите событие, которое хотите добавить в календарь.\nПримеры событий:\n"
+    hint_articles = [types.InlineQueryResultArticle(id='1', title='Ввод события', description=hint_msg, input_message_content=types.InputTextMessageContent('Пример: Праздничный обед в 12:00'))]
+    i = 2
+    for hint in Config.instance().message_examples_hint:
+        hint_articles.append(types.InlineQueryResultArticle(id=str(i), title="Событие " + str(i - 1), description=hint.encode('utf-8'),
+                                                            input_message_content=types.InputTextMessageContent(hint.encode('utf-8'))))
+        i += 1
+
+    try:
+        bot.answer_inline_query(inline_query.id, hint_articles)
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     # Removing webhook just for sure
